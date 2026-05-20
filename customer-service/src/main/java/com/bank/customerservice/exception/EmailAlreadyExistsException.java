c003e985578eaeed8194f627784f9df736e59c6c
Sharathreddysuram <sharathreddysuram@gmail.com>
2026-05-20 11:27:01 -0400

    Delete customer-service directory

--- customer-service/src/main/java/com/bank/customerservice/exception/EmailAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.bank.customerservice.exception;
-
-public class EmailAlreadyExistsException extends RuntimeException
-{
-    public EmailAlreadyExistsException(String message)
-    {
-        super(message);
-    }
-}
